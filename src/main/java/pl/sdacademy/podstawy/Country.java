7f46c9eed17d9cd0cdd184ea88bc875dbdf56e9f
Piotr Kachnowicz <p.kachnowicz@wp.pl>
2021-02-23 21:06:32 +0100

    2021-02-23 After adding toString method in Company.class

---
+++ src/main/java/pl/sdacademy/podstawy/Country.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.podstawy;
+
+public class Country {
+    private final int retirementAge;
+    private final String countryName;
+
+    public Country(int retirementAge, String countryName) {
+        this.retirementAge = retirementAge;
+        this.countryName = countryName;
+    }
+
+    public int getRetirementAge() {
+        return retirementAge;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String toString() {
+        return countryName;
+    }
+}
+// klasa niemodowalna
