842adb7dcf411edc128b1ad29e3f61f67bdcbfa3
Piotr Kachnowicz <p.kachnowicz@wp.pl>
2021-02-24 18:44:20 +0100

    2021-02-24 Adding employees to Company

--- src/main/java/pl/sdacademy/podstawy/Company.java
+++ src/main/java/pl/sdacademy/podstawy/Company.java
@@ -3,10 +3,12 @@ package pl.sdacademy.podstawy;
 public class Company {
     private final Country country;
     private final String companyName;
+    private Employee[] employees;
 
     public Company(Country country, String companyName) {
         this.country = country;
         this.companyName = companyName;
+        this.employees = new Employee[0];
     }
 
     public Country getCountry() {
@@ -15,6 +17,16 @@ public class Company {
 
     public String getCompanyName() {
         return companyName;
+    }
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Employee[] employees) {
+        if (employees != null) {
+            this.employees = employees;
+        }
     }
 
     public String toString() {
